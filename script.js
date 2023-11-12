76f9ddd2b42e10733ab238d366a5fc1012ad57e8
Bartek <118764028+barteqcz@users.noreply.github.com>
2023-11-12 20:32:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('.wholesite').on('click', '.playbtn', function () {
+        changePlayBtn(this);
+        playRadio(this);
+    });
+});
+
+function playRadio(audioId) {
+    var audio = document.getElementById(audioId);
+
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+      location.reload(true);
+    }
+}
+
+function changePlayBtn(clickedButton) {
+    $(clickedButton).toggleClass("fa-play fa-pause");
+}
+
+function normalizeString(str) {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+}
+
+window.onload = function () {
+    const noStationsFound = document.getElementById('noStationsFound');
+
+    for (const a of document.getElementsByTagName('audio')) {
+        a.src += '?cachebust=' + (+new Date());
+    }
+
+    document.querySelector('.searchbar').addEventListener('input', function () {
+        const searchTerm = normalizeString(this.value);
+        let stationsFound = false;
+
+        document.querySelectorAll('.box').forEach(function (box) {
+            const radioName = normalizeString(box.querySelector('h1').innerText);
+
+            if (radioName.includes(searchTerm)) {
+                box.style.display = 'flex';
+                stationsFound = true;
+            } else {
+                box.style.display = 'none';
+            }
+        });
+        
+        noStationsFound.style.display = stationsFound ? 'none' : 'block';
+    });
+}
